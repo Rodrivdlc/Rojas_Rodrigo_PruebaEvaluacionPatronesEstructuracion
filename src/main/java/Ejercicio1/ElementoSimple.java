aa4654f1aa696ade17412d44367f8b83293647b7
Rodrivdlc <rrojared@myuax.com>
2024-04-15 14:59:45 +0200

    javadoc 1 clase ElementoSimple

--- src/main/java/Ejercicio1/ElementoSimple.java
+++ src/main/java/Ejercicio1/ElementoSimple.java
@@ -1,5 +1,9 @@
 package src.main.java.Ejercicio1;
 
+/**
+ * La clase ElementoSimple representa un elemento simple en el sistema.
+ * Implementa la interfaz Elemento y tiene dos atributos: precio y codigo.
+ */
 public class ElementoSimple implements Elemento {
     private double precio;
     private int codigo;
