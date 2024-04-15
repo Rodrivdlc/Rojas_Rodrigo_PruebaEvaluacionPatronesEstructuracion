ed5dbc105031a5d590f3585ce8c241b66a0be49d
Rodrivdlc <rrojared@myuax.com>
2024-04-15 15:04:09 +0200

    javadoc class kit

--- src/main/java/Ejercicio1/Kit.java
+++ src/main/java/Ejercicio1/Kit.java
@@ -2,19 +2,33 @@ package src.main.java.Ejercicio1;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * La clase Kit representa un kit en el sistema.
+ * Implementa la interfaz Elemento y tiene una lista de elementos y un codigo.
+ */
 public class Kit implements Elemento {
     private List<Elemento> elementos = new ArrayList<>();
     private int codigo;
 
+    /**
+     * Constructor de la clase Kit.
+     * @param codigo El codigo del kit.
+     */
     public Kit(int codigo) {
         this.codigo = codigo;
     }
-
+    /**
+     * Agrega un elemento al kit.
+     * @param elemento El elemento a agregar.
+     */
     public void addElemento(Elemento elemento) {
         elementos.add(elemento);
     }
-
+    /**
+     * Obtiene el precio del kit.
+     * El precio se calcula como la suma de los precios de los elementos con un descuento del 10%.
+     * @return El precio del kit.
+     */
     @Override
     public double getPrecio() {
         double precioTotal = 0;
@@ -23,7 +37,10 @@ public class Kit implements Elemento {
         }
         return precioTotal * 0.9; // Aplicamos el descuento del 10%
     }
-
+    /**
+     * Obtiene el codigo del kit.
+     * @return El codigo del kit.
+     */
     @Override
     public int getCodigo() {
         return codigo;
